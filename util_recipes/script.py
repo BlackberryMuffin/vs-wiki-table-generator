3093c1e04c8bb374a06dddf7165146e8a2acd9af
Philip <philip.dorfner@smail.inf.h-brs.de>
2026-03-11 06:58:51 +0100

    fuck recipes are gonna be disgusting

--- util_recipes/script.py
+++ util_recipes/script.py
@@ -9,8 +9,6 @@ To search for relevant shapes look for ".*	.*" (including the quotation marks) w
 Idk if this will cause problems yet.
 """
 
-
-
 def setup(install_path:str, output_path:str, *, lang:str="en", gen_cleaned_jsons:bool=True):
     global recipes
     recipes={}
@@ -27,9 +25,51 @@ def setup(install_path:str, output_path:str, *, lang:str="en", gen_cleaned_jsons
                     with open(entry.path, "r") as f:
                         recipes[sub_dirs[i][1]+entry.name[:-5]] = json.loads(repair_json(f.read()))
                     if gen_cleaned_jsons:
-                        path=output_path + "/generated/cleaned_jsons/"+"/".join(sub_dirs[i][1].split("-"))
+                        path = output_path + "/generated/cleaned_jsons/" + "/".join(sub_dirs[i][1].split("-"))
                         if not Path(path).exists():
                             Path(path).mkdir(parents=True, exist_ok=True)
                         with open(path+entry.name, "x", encoding="utf-8") as fi:
                             fi.write(json.dumps(recipes[sub_dirs[i][1]+entry.name[:-5]], indent=4))
         i+=1
+    if gen_cleaned_jsons:
+        with open(output_path + "/generated/cleaned_jsons/_combined.json", "x", encoding="utf-8") as fi:
+            fi.write(json.dumps(recipes, indent=4))
+    recipes_by_type(destinction_fun=(lambda item: item.startswith("clothes-")))
+
+
+
+### destinction_fun expects a function to specify what kinda of items are wanted. It should be able to receive 1 string parameter e.g.:
+###   `recipes_by_type(destinction_fun=(lambda item: item.startswith("clothes-")))` to narrow it down to just clothing items.
+def recipes_by_type(destinction_fun):
+    out={}
+    for recipe_category in recipes:
+        if destinction_fun(recipe_category):
+            for recipe in recipes[recipe_category]:
+                if recipe["output"]["code"] not in out:
+                    out[recipe["output"]["code"]] = []
+                out[recipe["output"]["code"]].append(recipe)
+    return out
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
